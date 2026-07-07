bc0bd38f6f432177dd07aeaa164787722971d931
Kirky-X <kirky-x@outlook.com>
2026-07-07 18:31:52 +0800

    chore: untrack specmark, remove MIT copyright headers from src/tests

--- tests/integration_validation.rs
+++ tests/integration_validation.rs
@@ -1,8 +1,3 @@
-// Copyright (c) 2025 Kirky.X
-//
-// Licensed under the MIT License
-// See LICENSE file in the project root for full license information.
-
 //! Integration tests for validation support.
 
 #![cfg(feature = "validation")]
